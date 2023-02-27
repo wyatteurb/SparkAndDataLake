4a66970e678e4a5ad2063347e82860def2aca80f
tneurb <tneurb@gmail.com>
2023-02-27 17:27:57 +0700

    first commit

---
+++ s3-sync-all.sh
@@ -0,0 +1,3 @@
+aws s3 sync ./project/customers/ s3://yourname-stedi-lakehouse/customer/landing
+aws s3 sync ./project/accelerometer/ s3://yourname-stedi-lakehouse/accelerometer/landing
+aws s3 sync ./project/step_trainer/ s3://yourname-stedi-lakehouse/step_trainer/landing
